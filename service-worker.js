0c0593035b63410e0ca6a76f2960791d5eb1aa94
alvibhuya043-byte <alvibhuya043@gmail.com>
2026-09-19 12:58:24 +0600

    Update service-worker.js
    Signed-off-by: alvibhuya043-byte <alvibhuya043@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,25 @@
-const CACHE='party-manager-v3';
-const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='party-manager-v4';
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r || fetch(e.request))
+  );
+});
