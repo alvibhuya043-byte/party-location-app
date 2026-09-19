b483997624c605f3494499d48c083108c623d04e
alvibhuya043-byte <alvibhuya043@gmail.com>
2026-09-19 11:04:28 +0600

    Add files via upload
    Signed-off-by: alvibhuya043-byte <alvibhuya043@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-const CACHE='party-manager-v2'; self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])))); self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='party-manager-v3';
+const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
